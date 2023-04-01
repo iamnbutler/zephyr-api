55c8479f8d1e030a82b170697f16176c4285d6b9
Nate Butler <iamnbutler@gmail.com>
2023-04-01 12:41:08 -0400

    tree-sitter hello world

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use tree_sitter::{Language, Parser, Query, QueryCursor};
+
+fn process_input() -> () {
+    let mut parser = Parser::new();
+    let rust_lang: Language = tree_sitter_rust::language();
+    parser.set_language(rust_lang).unwrap();
+
+    let code = r#"
+            fn add(a: i32, b: i32) -> i32 {
+                a + b
+            }
+        "#;
+
+    let tree = parser.parse(&code, None).unwrap();
+
+    let query_string = "(function_item) @function";
+    let query = Query::new(rust_lang, &query_string).unwrap();
+
+    let mut query_cursor = QueryCursor::new();
+    let all_matches = query_cursor.matches(&query, tree.root_node(), code.as_bytes());
+
+    for each_match in all_matches {
+        for capture in each_match.captures.iter() {
+            println!("Capture: {:?}", capture);
+        }
+    }
+}
+
+fn main() {
+    process_input();
+}
