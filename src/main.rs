2fc247296ddd204410f7464666513d44fe7bb63f
Nate Butler <iamnbutler@gmail.com>
2023-04-01 12:50:46 -0400

    Print strings instead of logging the capture

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tree_sitter::{Language, Parser, Query, QueryCursor};
 
-fn process_input() -> () {
+fn process_input() -> Vec<String> {
     let mut parser = Parser::new();
     let rust_lang: Language = tree_sitter_rust::language();
     parser.set_language(rust_lang).unwrap();
@@ -23,13 +23,23 @@ fn process_input() -> () {
     let mut query_cursor = QueryCursor::new();
     let all_matches = query_cursor.matches(&query, tree.root_node(), code.as_bytes());
 
+    let mut captured_functions: Vec<String> = Vec::new();
+
     for each_match in all_matches {
         for capture in each_match.captures.iter() {
-            println!("Capture: {:?}", capture);
+            let start_byte = capture.node.start_byte();
+            let end_byte = capture.node.end_byte();
+            let function_text = &code[start_byte..end_byte];
+            captured_functions.push(function_text.to_string());
         }
     }
+
+    captured_functions
 }
 
 fn main() {
-    process_input();
+    let captured_functions = process_input();
+    for function_text in captured_functions {
+        println!("{}", function_text);
+    }
 }
