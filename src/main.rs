a2b40b89ced1e2c0b1de5b3c79033074ffe2bb13
Nate Butler <iamnbutler@gmail.com>
2023-04-04 15:00:10 -0400

    Push non-captured elements as "none"

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,13 @@ fn process_input() -> Vec<String> {
     parser.set_language(rust_lang).unwrap();
 
     let mut code = r#"
-use theme::ThemeRegistry;
-use util::http::HttpClient;
-use util::{paths, ResultExt, StaffMode};
-
-/// This is a comment
-const SERVER_PATH: &'static str =
-    /// This is another comment
-    "node_modules/vscode-json-languageserver/bin/vscode-json-languageserver";
-
-fn server_binary_arguments(server_path: &Path) -> Vec<OsString> {
-    vec![server_path.into(), "--stdio".into()]
-}
-
-pub struct JsonLspAdapter {
-    node: Arc<NodeRuntime>,
-    languages: Arc<LanguageRegistry>,
-    themes: Arc<ThemeRegistry>,
-}
-        "#;
+        unsafe {
+            let dst = s.as_mut_ptr().add(range.start);
+            let src = s.as_ptr().add(range.end);
+            let count = range_end - range.end;
+            std::ptr::copy(src, dst, count);
+            println!("{}", s);
+        }"#;
 
     let tree = parser.parse(&code, None).unwrap();
     let query_string = fs::read_to_string("./src/language/rust/highlights.scm")
@@ -39,14 +27,20 @@ pub struct JsonLspAdapter {
 
     let mut captured_elements: Vec<String> = Vec::new();
 
-    // dbg!(&query.capture_names()[1]);
+    let mut last_capture_end_byte = 0;
     for (mat, ix) in captures {
         let capture = &mat.captures[ix];
         let capture_name = &query.capture_names()[capture.index as usize];
         let start_byte = capture.node.start_byte();
         let end_byte = capture.node.end_byte();
         let element_text = &code[start_byte..end_byte];
-        captured_elements.push(format!("{} - {}", capture_name, element_text));
+        if start_byte > last_capture_end_byte {
+            let text = &code[last_capture_end_byte..start_byte];
+            captured_elements.push(format!("Type: (none), Text: {}", text));
+        }
+        last_capture_end_byte = end_byte;
+
+        captured_elements.push(format!("Type: {}:, Text: {}", capture_name, element_text));
     }
 
     captured_elements
