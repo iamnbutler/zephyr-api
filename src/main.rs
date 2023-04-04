25620378bde018122702de92bca40439fed9c06d
Nate Butler <iamnbutler@gmail.com>
2023-04-04 15:55:48 -0400

    Structure nodes to be exported as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
+use serde_derive::{Deserialize, Serialize};
 use std::fs;
-
 use tree_sitter::{Language, Parser, Query, QueryCursor};
 
-fn process_input() -> Vec<String> {
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Node {
+    pub node_type: String,
+    pub element_text: String,
+}
+
+impl Default for Node {
+    fn default() -> Node {
+        Node {
+            node_type: String::from("none"),
+            element_text: String::from(""),
+        }
+    }
+}
+
+fn process_input() -> Vec<Node> {
     let mut parser = Parser::new();
     let rust_lang: Language = tree_sitter_rust::language();
     parser.set_language(rust_lang).unwrap();
 
-    let code = r#"
-unsafe {
+    let code = r#"unsafe {
     let dst = s.as_mut_ptr().add(range.start);
     let src = s.as_ptr().add(range.end);
     let count = range_end - range.end;
@@ -25,10 +39,11 @@ unsafe {
     let mut query_cursor = QueryCursor::new();
     let captures = query_cursor.captures(&query, tree.root_node(), code.as_bytes());
 
-    let mut captured_elements: Vec<String> = Vec::new();
+    let mut captured_elements: Vec<Node> = Vec::new();
 
     let mut last_capture_end_byte = 0;
     for (mat, ix) in captures {
+        let mut node: Node = Node::default();
         let capture = &mat.captures[ix];
         let capture_name = &query.capture_names()[capture.index as usize];
         let start_byte = capture.node.start_byte();
@@ -36,10 +51,13 @@ unsafe {
         let element_text = &code[start_byte..end_byte];
         if start_byte > last_capture_end_byte {
             let text = &code[last_capture_end_byte..start_byte];
-            captured_elements.push(format!("Type: (none), Text: {}", text));
+            node.element_text = String::from(text);
+            captured_elements.push(node.clone());
         }
         last_capture_end_byte = end_byte;
-        captured_elements.push(format!("Type: {}:, Text: {}", capture_name, element_text));
+        node.element_text = String::from(element_text);
+        node.node_type = String::from(capture_name);
+        captured_elements.push(node);
     }
 
     captured_elements
@@ -47,7 +65,6 @@ unsafe {
 
 fn main() {
     let captured_elements = process_input();
-    for element in captured_elements {
-        println!("{}", element);
-    }
+    let json_string = serde_json::to_string(&captured_elements).unwrap();
+    println!("{}", json_string);
 }
